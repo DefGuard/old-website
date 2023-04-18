aa4cda33d1764dded53a43d888f8633891c9d29b
Filip Slezak <fslezak@teonite.com>
2023-04-18 13:02:19 +0200

    testimonials slider

--- assets/scripts/landing/main.ts
+++ assets/scripts/landing/main.ts
@@ -2,6 +2,8 @@ import "./src/animations/githubButton";
 import "./src/animations/linkButton";
 import navigation from "./src/navigation";
 import socials from "./src/socials";
+import testimonials from "./src/testimonials";
 
 socials();
 navigation();
+testimonials();
